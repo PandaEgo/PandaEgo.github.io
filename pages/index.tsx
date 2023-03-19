05ec97739a072b29ba33184d9145b651d5089f84
PandaEgo <panda.ego@chenzhe.dev>
2023-03-20 01:01:00 +0800

    初始化

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Panda Ego</title>
+        <meta name="description" content="Panda 的自留地" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Hi~ here is PandaEgo</h1>
+      </main>
+    </>
+  )
+}
